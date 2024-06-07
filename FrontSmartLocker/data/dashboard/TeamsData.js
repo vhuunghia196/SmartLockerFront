807786b95fcc082c1375e53b0ac228d7be18ffac
Võ Hữu Nghĩa <nghiabongda2@gmail.com>
2024-06-07 21:26:29 +0700

    Add Front

---
+++ FrontSmartLocker/data/dashboard/TeamsData.js
@@ -0,0 +1,20 @@
+export const TeamsData = [
+    {
+       id:1,
+       name : "Anita Parmar",
+       email : "anita@example.com",
+       role: "Front End Developer",
+       lastActivity: '3 May, 2023',
+       image:'/images/avatar/avatar-2.jpg' 
+    },
+    {
+        id:2,
+        name : "Jitu Chauhan",
+        email : "jituchauhan@example.com",
+        role: "Project Director",
+        lastActivity: 'Today',
+        image:'/images/avatar/avatar-1.jpg' 
+     }
+
+];
+export default TeamsData;
