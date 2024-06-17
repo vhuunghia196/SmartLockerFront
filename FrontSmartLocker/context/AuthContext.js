a3178a7ae24344c31e40245a7a497dc10f61fd6a
Võ Hữu Nghĩa <nghiabongda2@gmail.com>
2024-06-17 14:39:00 +0700

    add crud table, chart

--- FrontSmartLocker/context/AuthContext.js
+++ FrontSmartLocker/context/AuthContext.js
@@ -9,23 +9,6 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const router = useRouter();
 
-  // useEffect(() => {
-  //   // Kiểm tra nếu user không tồn tại và không phải ở trang đăng nhập, thực hiện chuyển hướng đến trang đăng nhập
-  //   if (!user) {
-  //       if(router.pathname === '/authentication/sign-in')
-  //           router.push('/authentication/sign-in');
-  //       if(router.pathname === '/authentication/sign-up')
-  //           router.push('/authentication/sign-up');     
-  //       if(router.pathname === '/authentication/forget-password')
-  //           router.push('/authentication/forget-password');     
-
-  //   }
-  //   // Kiểm tra nếu user đã đăng nhập và đang cố gắng truy cập trang đăng nhập, thì chuyển hướng họ đến trang chính
-  //   if (user && (router.pathname === '/authentication/sign-in' || router.pathname === '/authentication/sign-up' || router.pathname === '/authentication/forget-password')) {
-  //     router.push('/');
-  //   }
-  // }, [user, router]);
-
   const login = (userData) => {
     setIsLoggedIn(true);
     setUser(userData);
